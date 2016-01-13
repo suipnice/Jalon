bdbda798c5a10d5f91730c8d1db5496b5ab95cee
Christophe BORDONADO <bordonad@unice.fr>
2016-01-13 11:19:56 +0100

    Rajout des informations pour la consultation de Elasticsearch, Mise en place de la structure pour l'onglet Extraits

--- jalon.wowza/jalon/wowza/content/jalonwowza.py
+++ jalon.wowza/jalon/wowza/content/jalonwowza.py
@@ -14,19 +14,29 @@ LOG = getLogger('[JalonWowza]')
 class JalonWowza(SimpleItem):
 
     implements(IJalonWowza)
-    _wowza_server = "http://domainname.com/"
+
+    _wowza_server = "http://domainname.com"
     _wowza_application = "vod"
     _wowza_secret = ""
     _wowza_sha = "512"
+
+    _pod_server = "http://domainname.com"
+    _pod_user = "wowza"
+    _pod_elasticsearch_port = 9200
+    _pod_elasticsearch_index = "pod"
 
     def __init__(self, *args, **kwargs):
         super(JalonWowza, self).__init__(*args, **kwargs)
 
     def getWowzaProperties(self):
-        return {"wowza_server":      self._wowza_server,
-                "wowza_application": self._wowza_application,
-                "wowza_secret":      self._wowza_secret,
-                "wowza_sha":         self._wowza_sha}
+        return {"wowza_server":            self._wowza_server,
+                "wowza_application":       self._wowza_application,
+                "wowza_secret":            self._wowza_secret,
+                "wowza_sha":               self._wowza_sha,
+                "pod_server":              self._pod_server,
+                "pod_user":                self._pod_user,
+                "pod_elasticsearch_port":  self._pod_elasticsearch_port,
+                "pod_elasticsearch_index": self._pod_elasticsearch_index}
 
     def setWowzaProperties(self, wowzaProperties):
         for key in wowzaProperties.keys():
