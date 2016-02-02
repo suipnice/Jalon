10197b9d723be38a10fd423e1e923db7026745a9
Christophe BORDONADO <bordonad@unice.fr>
2016-02-02 11:54:01 +0100

    Simplification du code template ajout d'une première view

--- jalon.content/jalon/content/content/jalonfolder.py
+++ jalon.content/jalon/content/content/jalonfolder.py
@@ -325,6 +325,41 @@ class JalonFolder(ATFolder):
         c = a.difference(b)
         return c
 
+    def getMesCoursView(self, tab, user):
+        LOG.info("----- getMesCoursView -----")
+
+        folder_link = self.absolute_url()
+        is_manager = user.has_role("Manager")
+        actions_list = [{"css_class":   "button create expand",
+                         "action_link": "%s/folder_form?macro=macro_mescours&amp;formulaire=ajout-cours" % folder_link,
+                         "action_icon": "fa fa-plus-circle",
+                         "action_name": "Créer un cours"},
+                        {"css_class":   "button",
+                         "action_link": "%s/lister_auteur" % folder_link,
+                         "action_icon": "fa fa-code-fork",
+                         "action_name": "Dupliquer un cours"}]
+        if not is_manager:
+            del actions_list[-1]
+
+        tabs_list = [{"css_class": "button small selected" if tab == "1" else "button small secondary",
+                      "tab_link":  "%s?onglet=1" % folder_link,
+                      "tab_name":  "Favoris"},
+                     {"css_class": "button small selected" if tab == "2" else "button small secondary",
+                      "tab_link":  "%s?onglet=2" % folder_link,
+                      "tab_name":  "Auteur"},
+                     {"css_class": "button small selected" if tab == "3" else "button small secondary",
+                      "tab_link":  "%s?onglet=3" % folder_link,
+                      "tab_name":  "Co-auteur"},
+                     {"css_class": "button small selected" if tab == "4" else "button small secondary",
+                      "tab_link":  "%s?onglet=4" % folder_link,
+                      "tab_name":  "Lecteur"},
+                     {"css_class": "button small selected" if tab == "5" else "button small secondary",
+                      "tab_link":  "%s?onglet=5" % folder_link,
+                      "tab_name":  "Archives"}]
+        return {"is_manager":   is_manager,
+                "actions_list": actions_list,
+                "tabs_list":    tabs_list}
+
     def getInfosCours(self, cours_brain, authors_dict, member_id, member_login_time, onglet, actions_list):
         LOG.info("----- getInfosCours -----")
 
