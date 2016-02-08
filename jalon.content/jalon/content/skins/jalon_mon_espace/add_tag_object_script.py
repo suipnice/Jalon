9cbc1dd45d0256d1646111a695e0ea3d8e930a1d
Christophe BORDONADO <bordonad@unice.fr>
2016-02-08 12:05:06 +0100

    étiqueter un objet, bug JS à corriger

---
+++ jalon.content/jalon/content/skins/jalon_mon_espace/add_tag_object_script.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+## Controller Python Script "add_tag_object_script"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+try:
+    tags_list = tuple(context.REQUEST.form["listeTag"])
+except:
+    tags_list = ()
+context.setSubject(tags_list)
+context.reindexObject()
+
+redirection = context.absolute_url()
+if context.REQUEST.HTTP_X_REQUESTED_WITH != 'XMLHttpRequest':
+    context.REQUEST.RESPONSE.redirect(redirection)
+else:
+    return redirection
