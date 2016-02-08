209aabc706546e336e686de48091538dd69ba7bb
Philippe Pomédio <philippe.pomedio@unice.fr>
2016-02-08 16:39:55 +0100

    Affichage réponse (marquage HTML) si appel AJAX
    Par CB suite à séance de programmation en binôme (pair programming).

--- jalon.content/jalon/content/skins/jalon_mon_espace/add_tag_object_script.py
+++ jalon.content/jalon/content/skins/jalon_mon_espace/add_tag_object_script.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-## Controller Python Script "add_tag_object_script"
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=
-##title=
+# Controller Python Script "add_tag_object_script"
+# bind container=container
+# bind context=context
+# bind namespace=
+# bind script=script
+# bind subpath=traverse_subpath
+# parameters=
+# title=
 ##
 
 try:
@@ -17,7 +17,9 @@ context.setSubject(tags_list)
 context.reindexObject()
 
 redirection = context.absolute_url()
+# context.REQUEST.RESPONSE.redirect(redirection)
+
 if context.REQUEST.HTTP_X_REQUESTED_WITH != 'XMLHttpRequest':
     context.REQUEST.RESPONSE.redirect(redirection)
 else:
-    return redirection
+    return context.restrictedTraverse("mon_espace/mes_fichiers")()
