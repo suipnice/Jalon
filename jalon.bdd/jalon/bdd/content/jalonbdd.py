e6b462fca0049fb14f76c95b290d06c8b72f6914
Christophe BORDONADO <bordonad@unice.fr>
2016-02-15 11:22:55 +0100

    Correction Bug affichage des stats d'une ressource quand consultation de l'auteur

--- jalon.bdd/jalon/bdd/content/jalonbdd.py
+++ jalon.bdd/jalon/bdd/content/jalonbdd.py
@@ -1172,28 +1172,27 @@ class JalonBDD(SimpleItem):
         elements_consultation = jalon_mysql.getConsultationByElementsByCoursByUniversityYear(session, ID_COURS, year, elements_list)
         for ligne in elements_consultation.all():
             #LOG.info(ligne)
-            #try:
-            consultation_dict[ligne[0]]["nb_cons_year"] = ligne[1]
-            if not consultation_dict[ligne[0]] in consultations_list:
-                consultations_list.append(consultation_dict[ligne[0]])
-            #except:
-            #    pass
+            try:
+                consultation_dict[ligne[0]]["nb_cons_year"] = ligne[1]
+                if not consultation_dict[ligne[0]] in consultations_list:
+                    consultations_list.append(consultation_dict[ligne[0]])
+            except:
+                pass
         #LOG.info(consultation_dict)
 
         elements_consultation_month_before = jalon_mysql.getConsultationByElementsByCoursByMonth(session, ID_COURS, monthPrec, yearPrec, elements_list)
         for ligne in elements_consultation_month_before.all():
-            #try:
-            consultation_dict[ligne[0]]["nb_cons_month_before"] = ligne[1]
-            #except:
-            #    consultation_dict[ligne[0]]["nb_cons_month_before"] = 0
-            #    pass
+            try:
+                consultation_dict[ligne[0]]["nb_cons_month_before"] = ligne[1]
+            except:
+                pass
 
         elements_consultation_month = jalon_mysql.getConsultationByElementsByCoursByMonth(session, ID_COURS, month, year, elements_list)
         for ligne in elements_consultation_month.all():
-            #try:
-            consultation_dict[ligne[0]]["nb_cons_month"] = ligne[1]
-            #except:
-            #    consultation_dict[ligne[0]]["nb_cons_month"] = 0
+            try:
+                consultation_dict[ligne[0]]["nb_cons_month"] = ligne[1]
+            except:
+                pass
 
         #LOG.info(consultations_list)
         for consultation in consultations_list:
