e19d4d7bda96beda4530173cd53411b8f93c7309
Christophe BORDONADO <bordonad@unice.fr>
2016-02-17 11:40:41 +0100

    Ajout d'un cours OK

--- jalon.content/jalon/content/browser/scripts/my_courses_view.py
+++ jalon.content/jalon/content/browser/scripts/my_courses_view.py
@@ -35,17 +35,18 @@ class MyCoursesView(BrowserView):
         LOG.info("----- getMyCoursesView -----")
 
         folder = self.getUserFolder(user.getId())
-        folder_link = self.context.absolute_url()
+        folder_link = folder.absolute_url()
+        context_link = self.context.absolute_url()
 
         is_manager = user.has_role("Manager")
         is_student = user.has_role("Etudiant") or user.has_role("EtudiantJalon")
 
         actions_list = [{"css_class":   "button create expand",
-                         "action_link": "%s/folder_form?macro=macro_mescours_actions&amp;formulaire=add_course" % folder_link,
+                         "action_link": "%s/create_course_form" % folder_link,
                          "action_icon": "fa fa-plus-circle",
                          "action_name": "Créer un cours"},
                         {"css_class":   "button",
-                         "action_link": "%s/lister_auteur" % folder_link,
+                         "action_link": "%s/lister_auteur" % context_link,
                          "action_icon": "fa fa-code-fork",
                          "action_name": "Dupliquer un cours"}]
         if not is_manager:
@@ -54,23 +55,23 @@ class MyCoursesView(BrowserView):
         if not tab:
             tab = "1" if self.context.getComplement() == "True" else "2"
         tabs_list = [{"css_class": "button small selected" if tab == "1" else "button small secondary",
-                      "tab_link":  "%s?onglet=1" % folder_link,
+                      "tab_link":  "%s?onglet=1" % context_link,
                       "tab_icon":  "fa fa-star fa-fw",
                       "tab_name":  "Favoris"},
                      {"css_class": "button small selected" if tab == "2" else "button small secondary",
-                      "tab_link":  "%s?onglet=2" % folder_link,
+                      "tab_link":  "%s?onglet=2" % context_link,
                       "tab_icon":  "fa fa-user fa-fw",
                       "tab_name":  "Auteur"},
                      {"css_class": "button small selected" if tab == "3" else "button small secondary",
-                      "tab_link":  "%s?onglet=3" % folder_link,
+                      "tab_link":  "%s?onglet=3" % context_link,
                       "tab_icon":  "fa fa-users fa-fw",
                       "tab_name":  "Co-auteur"},
                      {"css_class": "button small selected" if tab == "4" else "button small secondary",
-                      "tab_link":  "%s?onglet=4" % folder_link,
+                      "tab_link":  "%s?onglet=4" % context_link,
                       "tab_icon":  "fa fa-graduation-cap fa-fw",
                       "tab_name":  "Lecteur"},
                      {"css_class": "button small selected" if tab == "5" else "button small secondary",
-                      "tab_link":  "%s?onglet=5" % folder_link,
+                      "tab_link":  "%s?onglet=5" % context_link,
                       "tab_icon":  "fa fa-folder fa-fw",
                       "tab_name":  "Archives"}]
 
---
+++ jalon.content/jalon/content/skins/jalon_mescours/create_course_script.py
@@ -0,0 +1,28 @@
+## Controller Python Script "create_course_script"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+param = {}
+REQUEST = context.REQUEST
+
+idobj = context.invokeFactory(type_name='JalonCours', id="Cours-%s-%s" % (REQUEST["member_id"], DateTime().strftime("%Y%m%d%H%M%S")))
+
+obj = getattr(context, idobj)
+param = {"Title": REQUEST["title"], "Description": REQUEST["description"]}
+
+obj.setProperties(param)
+obj.invokeFactory(type_name='Folder', id="annonce")
+obj.invokeFactory(type_name='Ploneboard', id="forum")
+forum = getattr(obj, "forum")
+forum.setTitle("Liste des forums du cours")
+obj.creerSousObjet("Forum", "Discussion générale", "Discutez ici librement du cours.", REQUEST["member_id"], "", "")
+
+obj.setProperties({"DateDerniereModif": DateTime()})
+
+context.REQUEST.RESPONSE.redirect("%s?onglet=2" % context.absolute_url())
