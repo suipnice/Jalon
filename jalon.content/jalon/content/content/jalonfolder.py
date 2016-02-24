79dac3af4b8946cbd4279ca299f3779a16b2f76b
Christophe BORDONADO <bordonad@unice.fr>
2016-02-24 15:36:06 +0100

    Ajout étiquette OK

--- jalon.content/jalon/content/content/jalonfolder.py
+++ jalon.content/jalon/content/content/jalonfolder.py
@@ -1000,7 +1000,8 @@ class JalonFolder(ATFolder):
         member_id = portal.portal_membership.getAuthenticatedMember().getId()
         home = getattr(portal.Members, member_id)
 
-        folder_dict = {"mes_fichiers": "Fichiers"}
+        folder_dict = {"mes_fichiers":                 "Fichiers",
+                       "mes_presentations_sonorisees": "Sonorisation"}
         folder = getattr(home, folder_dict[self.getId()])
         if not tag in folder.Subject():
             tags = list(folder.Subject())
