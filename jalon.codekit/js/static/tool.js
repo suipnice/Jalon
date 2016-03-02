1ca97885fb9e94ea3600717d6080228fb1fe367e
Philippe Pomédio <philippe.pomedio@unice.fr>
2016-03-02 14:22:32 +0100

    Add base64 encoded images in forums posts.
    Note: base64 code seems to be removed by forum component…

--- jalon.codekit/js/static/tool.js
+++ jalon.codekit/js/static/tool.js
@@ -18,13 +18,14 @@ function instantiateCKEditor( textareaID ) {
             customConfig: '',
             language: 'fr',
             toolbarGroups: [
+                //{ 'name': "document",    'groups': [ "mode" ] },
                 { 'name': "basicstyles", 'groups': [ "basicstyles" ] },
                 { 'name': "clipboard",   'groups': [ "clipboard", "undo" ] },
                 { 'name': "links",       'groups': [ "links" ] },
                 { 'name': "paragraph",   'groups': [ "list" ] },
-                { 'name': "insert",      'groups': [ "insert" ] },
-                //{ 'name': "document",    'groups': [ "mode" ] }
+                { 'name': "insert",      'groups': [ "insert" ] }
             ],
+            removePlugins: 'base64image',
             removeButtons: 'Cut,Copy,Undo,Redo,Strike,Subscript,Superscript,Anchor',
             removeDialogTabs: 'link:advanced',
             pasteFromWordPromptCleanup: false,
