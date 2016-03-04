5e502c8ba4fc92a7b23a6e009bdbf7bb54a243eb
Christophe BORDONADO <bordonad@unice.fr>
2016-03-04 10:28:23 +0100

    Titre du cours + edit et accès public ok

--- jalon.content/jalon/content/browser/view_script/jalon_course_view.py
+++ jalon.content/jalon/content/browser/view_script/jalon_course_view.py
@@ -31,3 +31,22 @@ class JalonCourseView(BrowserView):
                 {"title": self.context.Title(),
                  "icon":  "fa fa-book",
                  "link":  self.context.absolute_url()}]
+
+    def getCourseView(self, user, mode_etudiant):
+        course_acces = self.context.getAcces()
+
+        return {"is_personnel": self.isPersonnel(user, mode_etudiant),
+                "is_public":    "success" if course_acces == "Public" else "disabled"}
+
+    def isPersonnel(self, user, mode_etudiant="false"):
+        #self.plone_log("jaloncours/isPersonnel")
+        if mode_etudiant == "true":
+            #self.plone_log("isPersonnel = False (mode étudiant)")
+            return False
+        if user.has_role("Manager"):
+            #self.plone_log("isPersonnel = True (manager role)")
+            return True
+        if user.has_role("Personnel") and self.context.isCoAuteurs(user.getId()):
+            #self.plone_log("isPersonnel = True (Personnel & iscoAuteurs)")
+            return True
+        return False
