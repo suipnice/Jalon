4541a87bc67b87a899d4f074761b9bfc9063b329
Christophe BORDONADO <bordonad@unice.fr>
2016-04-27 15:13:31 +0200

    Ajout d'un forum OK

---
+++ jalon.content/jalon/content/skins/jalon_content_course/add_course_forum_script.py
@@ -0,0 +1,19 @@
+##Python Script "add_course_announce_script"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+form = context.REQUEST.form
+
+context.addCourseForum(form["title"], form["description"], form["user_id"])
+
+HTTP_X_REQUESTED_WITH = True if context.REQUEST.HTTP_X_REQUESTED_WITH == 'XMLHttpRequest' else False
+if not HTTP_X_REQUESTED_WITH:
+    context.REQUEST.RESPONSE.redirect(context.absolute_url())
+else:
+    return context.absolute_url()
