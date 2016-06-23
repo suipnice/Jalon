88a3a909847500ce40b6e1ad3f606de0178d9aef
Christophe BORDONADO <bordonad@unice.fr>
2016-06-23 15:46:46 +0200

    Test sur mode_etudiant et non affichage des boutons pour les étudiants

--- jalon.content/jalon/content/browser/view_script/deposit_box_criteria_view.py
+++ jalon.content/jalon/content/browser/view_script/deposit_box_criteria_view.py
@@ -3,6 +3,7 @@ from Products.Five.browser import BrowserView
 from zope.component import getMultiAdapter
 
 from jalon.content import contentMessageFactory as _
+from DateTime import DateTime
 
 from logging import getLogger
 LOG = getLogger('[DepositBoxCriteriaView]')
@@ -36,18 +37,27 @@ class DepositBoxCriteriaView(BrowserView):
                  "icon":  "fa fa-inbox",
                  "link":  self.context.absolute_url()}]
 
-    def getCriteriaView(self):
+    def getCriteriaView(self, user, mode_etudiant):
         LOG.info("----- getCriteriaView (Start) -----")
         deposit_box = self.context
-        #deposit_box_id = deposit_box.getId()
+        is_personnel = self.context.isPersonnel(user, mode_etudiant)
+        mode_etudiant = "false" if (not mode_etudiant) and is_personnel else mode_etudiant
         my_view = {"is_anonymous":     self.isAnonymous(),
-                   "deposit_box_link": deposit_box.absolute_url()}
+                   "deposit_box_link": deposit_box.absolute_url(),
+                   "is_depot_actif":   True,
+                   "is_personnel":     is_personnel,
+                   "mode_etudiant":    mode_etudiant}
 
         my_view["criteria_dict"] = deposit_box.getCriteriaDict()
         my_view["criteria_order"] = deposit_box.getCriteriaOrder()
         my_view["comment_dict"] = {"0": "Aucun",
                                    "1": "Optionnel",
                                    "2": "Obligatoire"}
+
+        now = DateTime(DateTime()).strftime("%Y/%m/%d %H:%M")
+        date_depot = DateTime(deposit_box.getDateDepot()).strftime("%Y/%m/%d %H:%M")
+        if now >= date_depot:
+            my_view["is_depot_actif"] = False
         LOG.info("----- getCriteriaView (End) -----")
 
         return my_view
