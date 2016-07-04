9a277176056ea195c209e17d700bd50731a87d40
Olivier Bado <bado@unice.fr>
2016-07-04 17:24:09 +0200

    [WIMS] Ajoute l'ensemble de fichiers qui permettront l'édition des activités WIMS :   * edit_wims_activity_form.cpt (son contenu provient essentiellement de wims_activity_macro)   * edit_wims_activity_form.cpt.metadata (cf macro_form pour importer les anciennes données)   * validate_wims_activity_property.vpy (cf le validate de macro_form pour importer les anciennes données)   * edit_wims_activity_script.py (cf le script de macro_form pour importer les anciennes données)

---
+++ jalon.content/jalon/content/skins/jalon_content_course/edit_wims_activity_script.py
@@ -0,0 +1,28 @@
+## Controller Python Script "edit_deposit_box_script"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=Activite edit
+##
+
+#context = context
+form = context.REQUEST.form
+
+if "title" in form:
+    context.setProperties({"Title": form["title"]})
+else:
+    context.setProperties({"Profile": form["profile"]})
+
+# tab n'existe pas si on modifie l'activite depuis le plan du cours
+if "tab" in form:
+    redirection = "%s?tab=%s" % (context.absolute_url(), form["tab"])
+else:
+    redirection = context.aq_parent.absolute_url()
+
+if context.REQUEST.HTTP_X_REQUESTED_WITH != 'XMLHttpRequest':
+    context.REQUEST.RESPONSE.redirect(redirection)
+else:
+    return redirection
