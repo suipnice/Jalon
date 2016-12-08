bc9fed16e81e71db8004007c6b59cf8fdbc803a7
Olivier Bado <bado@unice.fr>
2016-12-08 17:35:39 +0100

    [i18n] Ajoute un fichier “Readme.md” qui explique comment générer le fichier .pot des traductions

--- jalon.content/jalon/content/locales/update_pot.sh
+++ jalon.content/jalon/content/locales/update_pot.sh
@@ -2,13 +2,13 @@ domain='jalon.content'
 excludes='profiles tests'
 
 home=$(eval echo ~${SUDO_USER})
-python_folder="$home"/Plone/Python-2.7/bin
+python_folder="$home"/Plone/zinstance/bin
 
 
-# Synchronise the templates and scripts with the .pot file for collective.quickupload domain.
-$python_folder/i18ndude rebuild-pot --pot $domain.pot --merge ./$domain-manual.pot --exclude="$excludes" --create $domain ../
+# Synchronise the templates and scripts with the .pot file for $domain.
+$python_folder/i18ndude rebuild-pot --pot "$domain.pot" --merge "./$domain-manual.pot" --exclude="$excludes" --create $domain "../"
 
-# Synchronise the $domain.po files (Used for the workflows)
+# Synchronise the $domain.po files
 $python_folder/i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 
 WARNINGS=`find ../ -name "*pt" | xargs $python_folder/i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
