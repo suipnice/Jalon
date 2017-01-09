ab0ece12620b6e15b5b5cc098b3454ccabea2bb4
Christophe BORDONADO <bordonad@unice.fr>
2017-01-09 15:15:28 +0100

    Options des compétences OK

---
+++ jalon.content/jalon/content/skins/jalon_content_course/edit_deposi_box_skills_options_script.py
@@ -0,0 +1,16 @@
+## Controller Python Script "edit_deposi_box_skills_options_script"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+form = context.REQUEST.form
+
+context.setProperties({"AfficherCompetences": int(form["AfficherCompetences"]),
+                       "ModifierCompetences": int(form["ModifierCompetences"])})
+
+context.REQUEST.RESPONSE.redirect("%s?tab=skills" % context.absolute_url())
