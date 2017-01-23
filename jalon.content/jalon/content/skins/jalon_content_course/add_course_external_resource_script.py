ba53ee4e75e0108afcbd298206329c2766f6d9f8
Christophe BORDONADO <bordonad@unice.fr>
2017-01-23 15:21:08 +0100

    Formulaire ajout lien depuis cours

--- jalon.content/jalon/content/skins/jalon_content_course/add_course_external_resource_script.py
+++ jalon.content/jalon/content/skins/jalon_content_course/add_course_external_resource_script.py
@@ -10,33 +10,39 @@
 
 form = context.REQUEST.form
 
-user_id = form["user_id"]
-portal = context.portal_url.getPortalObject()
-portal_workflow = portal.portal_workflow
-folder_object = context.getMySubSpaceFolder(user_id, "Externes", portal)
-
-external_id = "Externe-%s-%s" % (user_id, DateTime().strftime("%Y%m%d%H%M%S"))
-folder_object.invokeFactory(type_name="JalonRessourceExterne", id=external_id)
-external_object = getattr(folder_object, external_id)
-
-properties = {"Title":       form["title"],
-              "Description": form["description"]}
-
-if "iframe" in form["lien"] or "embed" in form["lien"]:
-    external_type = "Lecteur exportable"
-    properties["Lecteur"] = form["lien"]
+if form["title"] in ["", " ", None] or form["lien"] in ["", " ", None]:
+    redirection = "%s/create_and_add_course_external_resource_form" % context.absolute_url()
 else:
-    external_type = "Lien web"
-    properties["Urlbiblio"] = form["lien"]
-properties["TypeRessourceExterne"] = external_type
+    redirection = context.absolute_url()
+    user_id = form["user_id"]
+    portal = context.portal_url.getPortalObject()
+    portal_workflow = portal.portal_workflow
+    folder_object = context.getMySubSpaceFolder(user_id, "Externes", portal)
 
-external_object.setProperties(properties)
+    external_id = "Externe-%s-%s" % (user_id, DateTime().strftime("%Y%m%d%H%M%S"))
+    folder_object.invokeFactory(type_name="JalonRessourceExterne", id=external_id)
+    external_object = getattr(folder_object, external_id)
 
-display_item = DateTime() if "display_item" in form and form["display_item"] == "1" else ""
-display_in_plan = True if "display_in_plan" in form and form["display_in_plan"] == "1" else False
-map_items_position = form["map_position"] if form.has_key("map_position") else None
+    properties = {"Title":       form["title"],
+                  "Description": form["description"]}
 
-context.addMySpaceItem(folder_object, external_id, external_type, user_id, display_item, map_items_position, display_in_plan, portal_workflow)
+    if "iframe" in form["lien"] or "embed" in form["lien"]:
+        external_type = "Lecteur exportable"
+        properties["Lecteur"] = form["lien"]
+    else:
+        external_type = "Lien web"
+        properties["Urlbiblio"] = form["lien"]
+    properties["TypeRessourceExterne"] = external_type
 
-#context.REQUEST.RESPONSE.redirect(context.absolute_url())
-return "%s/display_course_map_page" % context.absolute_url()
+    external_object.setProperties(properties)
+
+    display_item = DateTime() if "display_item" in form and form["display_item"] == "1" else ""
+    display_in_plan = True if "display_in_plan" in form and form["display_in_plan"] == "1" else False
+    map_items_position = form["map_position"] if form.has_key("map_position") else None
+
+    context.addMySpaceItem(folder_object, external_id, external_type, user_id, display_item, map_items_position, display_in_plan, portal_workflow)
+
+    #context.REQUEST.RESPONSE.redirect(context.absolute_url())
+    #return "%s/display_course_map_page" % context.absolute_url()
+
+context.REQUEST.RESPONSE.redirect(redirection)
