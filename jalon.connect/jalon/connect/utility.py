f452c282848e0b1d71c040d91646e4e485bdeab8
Olivier Bado <bado@unice.fr>
2017-02-02 11:34:38 +0100

    [jalon.connect] PEP8+PEP257 sur utility.py + remplace des print par des LOG.info()

--- jalon.connect/jalon/connect/utility.py
+++ jalon.connect/jalon/connect/utility.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Adobe Connect Connector for Jalon LMS. """
+"""Adobe Connect Connector for Jalon LMS."""
 from zope.interface import implements
 from zope.schema.fieldproperty import FieldProperty
 from zope.component import getUtility
@@ -16,13 +16,16 @@ import re
 import urllib
 import urllib2
 import sys
-import logging
+
 from xml.etree.ElementTree import XML
 from DateTime import DateTime
 from urlparse import urlparse
 from time import strftime, gmtime
 
 from zope.interface import Invalid
+
+from logging import getLogger
+LOG = getLogger('[Jalon.Connect]')
 
 
 def form_adapter(context):
@@ -31,7 +34,6 @@ def form_adapter(context):
 
 
 class Connect(SimpleItem):
-
     u"""
     Connect Utility.
 
@@ -56,25 +58,27 @@ class Connect(SimpleItem):
 
     dossiers = FieldProperty(IConnectModele['dossiers'])
 
-    #debug = []
+    # debug = []
     # Parametres spécifiques
     session = None
 
     def getAttribut(self, param):
-        """ getAttribut renvoit l'attribut param["attribut"]. """
+        """getAttribut renvoit l'attribut param["attribut"]."""
         return self.__getattribute__(param["attribut"])
 
     def getConnectProperty(self, key):
+        """get Connect properties."""
         return getattr(self, "%s" % key)
 
     def setProperties(self, form):
+        """set properties."""
         for key in form.keys():
             val = form[key]
             if key.startswith("activer_"):
                 val = int(val)
             setattr(self, "%s" % key, val.decode("utf-8"))
 
-    ### METHODES PUBLIQUES ###########################################################
+    # ## METHODES PUBLIQUES ###########################################################
 
     def creerUser(self, params):
         """Cree un utilisateur connect.
@@ -83,7 +87,7 @@ class Connect(SimpleItem):
         params -- un dictionnaire contenant les attributs de l'utilisateur.
 
         """
-        #print params
+        # print params
         # Création du compte Connect de l'utilisateur
         firstname, lastname = params["fullname"].split(" ", 1)
         rep = self.requete({'action': 'principal-update',
@@ -98,7 +102,7 @@ class Connect(SimpleItem):
                             }, "principal/[0]")
 
         if not rep["error"]:
-            #Ajout de l'utilisateur au groupe "hotes de réunion" supprimé => inutile (les hotes de réunions peuvent créer de nouvelles réunions)
+            # Ajout de l'utilisateur au groupe "hotes de réunion" supprimé => inutile (les hotes de réunions peuvent créer de nouvelles réunions)
 
             # Recherche de l'ID de l'utilisateur
             idUser = rep["response"].attrib["principal-id"]
@@ -113,13 +117,13 @@ class Connect(SimpleItem):
         return rep["error"]
 
     def creerReunion(self, params):
-        """ creerReunion. """
-        #print "creerReunion"
+        """creerReunion."""
+        # print "creerReunion"
         # Recherche de l'id et du dossier de l'utilisateur
         idUser = self.rechercherIdUser(params["userid"])
-        #print "idUser : %s" % str(idUser)
+        # print "idUser : %s" % str(idUser)
         idFolder = self.rechercherDossierUser(params["userid"], "user-meetings")
-        #print "idFolder : %s" % str(idFolder)
+        # print "idFolder : %s" % str(idFolder)
 
         if idUser and idFolder:
             # Création des infos de la réunion
@@ -154,16 +158,16 @@ class Connect(SimpleItem):
                 urlReunion = self.xpath(meeting, "url-path/[0]").text
                 titleReunion = self.xpath(meeting, "name/[0]").text.encode("utf-8")
 
-                #print "@@@ AJOUT HOTE"
-                #print idReunion
+                # print "@@@ AJOUT HOTE"
+                # print idReunion
                 rep = self.requete({'action': 'permissions-update',
                                     'principal-id': idUser,
                                     'acl-id': idReunion,
                                     'permission-id': 'host'})
-                #print rep
+                # print rep
                 if not rep["error"]:
-                    #print "@@@ NOUVELLE REUNION"
-                    #print {"id" : idReunion, "title" : titleReunion, "url" : self.urlAbsolu(urlReunion) }
+                    # print "@@@ NOUVELLE REUNION"
+                    # print {"id" : idReunion, "title" : titleReunion, "url" : self.urlAbsolu(urlReunion) }
                     # Réunion publique
                     rep = self.requete({'action': 'permissions-update',
                                         'acl-id': idReunion,
@@ -178,19 +182,19 @@ class Connect(SimpleItem):
         return None
 
     def majPasswordUser(self, params):
-        """ majPasswordUser. """
-        #print "majPasswordUser"
+        """maj Password User."""
+        LOG.info("majPasswordUser")
         userid = self.rechercherIdUser(params["userid"])
         return self.requete({'action': 'user-update-pwd', 'user-id': userid, 'password': params["password"], 'password-verify': params["password"]}, 'status/code')
 
     def rechercherReunions(self, params):
-        """ rechercherReunions. """
-        print "@@@ RECHERCHER REUNIONS"
-        print params
+        """rechercher Reunions."""
+        LOG.info("@@@ RECHERCHER REUNIONS")
+
         # Vérification des paramètres
         if "login" in params:
             login = params["login"]
-            #pwd   = params["pass"]
+            # pwd = params["pass"]
 
             # Connexion admin
             if self.connexion():
@@ -208,27 +212,27 @@ class Connect(SimpleItem):
                 for meeting in meetings:
                     # Recherche du modèle
                     modele = meeting.attrib["source-sco-id"]
-                    if not modele in modeles:
+                    if modele not in modeles:
                         modeles[modele] = []
                         url = self.xpath(meeting, "url-path/[0]").text
                         modeles[modele].append({"id": meeting.attrib["sco-id"],
                                                 "title": self.xpath(meeting, "name/[0]").text.encode("utf-8"),
                                                 "url": self.urlAbsolu(url),
                                                 })
-                        #print "@@@ REUNION %s" % str({"id": meeting.attrib["sco-id"], "title": self.xpath(meeting, "name/[0]").text.encode("utf-8"),"url": self.urlAbsolu(url),})
-                #print "@@@ REUNIONS"
+                        # print "@@@ REUNION %s" % str({"id": meeting.attrib["sco-id"], "title": self.xpath(meeting, "name/[0]").text.encode("utf-8"),"url": self.urlAbsolu(url),})
+                # print "@@@ REUNIONS"
 
                 if params['modele'] in modeles:
-                    #print modeles[params['modele']]
+                    # print modeles[params['modele']]
                     return modeles[params['modele']]
                 else:
-                    #print modeles
+                    # print modeles
                     return None
         return None
 
     def rechercherEnregistrements(self, params):
-        """ rechercherEnregistrements."""
-        logger = logging.getLogger("Jalon.Connect[rechercherEnregistrements]")
+        """rechercherEnregistrements."""
+        LOG.info("rechercherEnregistrements")
 
         rep = self.requete({'action': 'sco-contents',
                             'sco-id': params['id'],
@@ -236,16 +240,16 @@ class Connect(SimpleItem):
                             'sort-date-created': 'desc'}, "scos/sco")
         if not rep["error"]:
             e = []
-            #logger.info("--Reponse de Connect : %s" % rep["response"])
+            # LOG.info("--Reponse de Connect : %s" % rep["response"])
             version = int(self.version)
-            #logger.info("--CONNECT version=%s" % version)
+            # LOG.info("--CONNECT version=%s" % version)
             for enregistrement in rep["response"]:
                 url = self.xpath(enregistrement, "url-path/[0]").text
-                #logger.info(url)
+                # LOG.info(url)
                 # Conversion de la durée
                 if version < 9:
                     duree = self.xpath(enregistrement, "duration/[0]")
-                    if not duree == None:
+                    if duree is not None:
                         duree = duree.text
                 else:
                     duree = enregistrement.attrib["duration"]
@@ -278,12 +282,12 @@ class Connect(SimpleItem):
                               })
             return e
         # Quand la ressource "sco-id demandee n'existe pas, connect8 peut renvoyer <status code="no-access" subcode="denied"/>
-        logger.error("reunion : %s -- retour : %s " % (params['id'], rep["error"]))
+        LOG.error("reunion : %s -- retour : %s " % (params['id'], rep["error"]))
         return []
 
     def genererSessionUser(self, params):
-        """ genererSessionUser. """
-        #logger = logging.getLogger("Jalon.Connect[genererSessionUser]")
+        """generer Session User."""
+        # LOG.info("genererSessionUser")
         url = "%s?action=login&login=%s&password=%s" % (self.url_connexion, params["userid"], params["password"])
         req = urllib2.Request(url)
         handle = urllib2.urlopen(req)
@@ -291,12 +295,12 @@ class Connect(SimpleItem):
         tab = cookie.split(";")
         return tab[0].split("=")[1]
 
-    ### METHODES PRIVEES ###########################################################
-    #security.declarePrivate('connexion')
+    # ## METHODES PRIVEES ###########################################################
+    # security.declarePrivate('connexion')
 
     def connexion(self, param=None):
-        """ connexion. """
-        #logger = logging.getLogger("Jalon.Connect[connexion]")
+        """connexion."""
+        # LOG.info("Jalon.Connect[connexion]")
 
         # Ré-init de la session
         self.session = None
@@ -315,7 +319,7 @@ class Connect(SimpleItem):
     security.declarePrivate('rechercherIdUser')
 
     def rechercherIdUser(self, loginUser):
-        """ rechercherIdUser. """
+        """rechercher Id User."""
         rep = self.requete({'action': 'principal-list',
                             'filter-login': loginUser
                             }, "principal-list/principal/[0]")
@@ -326,8 +330,7 @@ class Connect(SimpleItem):
     security.declarePrivate('rechercherShortcuts')
 
     def rechercherShortcuts(self, typesShortcuts):
-        """ rechercherShortcuts. """
-        # Recherche des raccourcis des dossiers
+        """Recherche des raccourcis des dossiers."""
         shortcuts = self.requete({'action': 'sco-shortcuts'}, "shortcuts/sco")
         s = []
         for typeShortcut in typesShortcuts:
@@ -343,7 +346,7 @@ class Connect(SimpleItem):
     security.declarePrivate('rechercherShortcuts')
 
     def rechercherDossierUser(self, login, shortcut):
-        """ rechercherDossierUser. """
+        """rechercher Dossier User."""
         # Recherche du raccourci
         idShortcut = self.rechercherShortcuts([shortcut])[0]
 
@@ -359,32 +362,32 @@ class Connect(SimpleItem):
     security.declarePrivate('supprimerEnregistrement')
 
     def supprimerEnregistrement(self, params):
-        """supprimerEnregistrement. """
-        #print params["idEnregistrement"]
+        """supprimer Enregistrement."""
+        # print params["idEnregistrement"]
         rep = self.requete({'action': 'sco-delete', 'sco-id': params["idEnregistrement"]})
         return not rep["error"]
 
-    ### UTILITAIRES ###########################################################
+    # ## UTILITAIRES ###########################################################
     security.declarePrivate('requete')
 
     def requete(self, params, xpath=None):
-        """ requete. """
-        #logger = logging.getLogger("Jalon.Connect[requete]")
+        """requete."""
+        # LOG.info("## requete ##")
         # Ajout du n° de session aux params
         if self.session is not None:
             params['session'] = self.session
-            #logger.info("--session = %s" % params['session'])
+            # LOG.info("--session = %s" % params['session'])
         # Requete au serveur
         data = urllib.urlencode(params)
         req = urllib2.Request("%s?%s" % (self.url_connexion, data))
-        #logger.info("REQUETE : %s?%s" % (self.url_connexion, data))
+        # LOG.info("REQUETE : %s?%s" % (self.url_connexion, data))
         try:
             handle = urllib2.urlopen(req)
             rep = handle.read()
         except:
             # Erreur HTTP
             raise Invalid(u"HTTP ; response : None ; error : %s" % str(sys.exc_info()))
-        #logger.info("REPONSE : %s" % rep)
+        # LOG.info("REPONSE : %s" % rep)
         # Conversion XML
         try:
             xml = XML(rep)
@@ -394,7 +397,7 @@ class Connect(SimpleItem):
         # Analyse de la réponse
         error = self.chercherErreur(xml)
         if not error:
-            #print tostring(xml)
+            # print tostring(xml)
             self.analyserReponse(params, xml)
             # Extraction si necessaire
             if xpath:
@@ -412,20 +415,20 @@ class Connect(SimpleItem):
     security.declarePrivate('chercherErreur')
 
     def chercherErreur(self, xml):
-        """ chercherErreur. """
-        logger = logging.getLogger("Jalon.Connect[chercherErreur]")
+        """chercher Erreur."""
+        # LOG.info("[chercherErreur]")
         # Recherche du statut
         statut = self.xpath(xml, "status/@code")
-        #logger.info("-- STATUT : %s" % statut)
+        # LOG.info("-- STATUT : %s" % statut)
 
         if statut == "invalid":
             field = self.xpath(xml, "status/invalid/@field")
             subcode = self.xpath(xml, "status/invalid/@subcode")
-            logger.error("ERREUR Connect / %s : %s" % (field, subcode))
+            LOG.error("ERREUR Connect / %s : %s" % (field, subcode))
             return "%s : %s" % (field, subcode)
         elif statut == "no-access":
             subcode = self.xpath(xml, "status/@subcode")
-            logger.error("ERREUR Connect / %s : %s" % (statut, subcode))
+            LOG.error("ERREUR Connect / %s : %s" % (statut, subcode))
             return {"error": "%s : %s" % (statut, subcode)}
         else:
             return None
@@ -433,16 +436,16 @@ class Connect(SimpleItem):
     security.declarePrivate('analyserReponse')
 
     def analyserReponse(self, params, xml):
-        """ analyserReponse. """
+        """analyser Reponse."""
         # Recherche du n° de session
         if not self.session and params['action'] == 'common-info':
             self.session = self.xpath(xml, "common/cookie/text()")
-            #print "--- SESSION : %s" % self.session
+            # print "--- SESSION : %s" % self.session
 
     security.declarePrivate('xpath')
 
     def xpath(self, xml, xpath):
-        """ xpath. """
+        """xpath."""
         # Conversion en liste
         xpaths = xpath.split('/')
         xpathparent = '/'.join(xpaths[0:-1])
@@ -471,16 +474,16 @@ class Connect(SimpleItem):
     security.declarePrivate('urlAbsolu')
 
     def urlAbsolu(self, url, session=None):
-        """ urlAbsolu. """
+        """urlAbsolu."""
         o = urlparse(self.url_connexion)
-        #return "%s://%s%s%s" % (o.scheme, o.netloc, url, "?session=%s" % self.session if session else "")
+        # return "%s://%s%s%s" % (o.scheme, o.netloc, url, "?session=%s" % self.session if session else "")
         return "%s://%s%s%s" % (o.scheme, o.netloc, url, "?session=%s" % session if session else "")
         # A faire : generer session user
 
     security.declarePrivate('convertirDate')
 
     def convertirDate(self, d, us=False):
-        """ convertirDate. """
+        """convertirDate."""
         if not us:
             return DateTime(d).strftime("%d.%m.%Y - %Hh%M")
         else:
@@ -489,7 +492,7 @@ class Connect(SimpleItem):
     security.declarePrivate('convertirDuree')
 
     def convertirDuree(self, d):
-        """ convertirDuree. """
+        """convertirDuree."""
         if d:
             m = re.match(r"(\d{2}):(\d{2}):(\d{2})\.\d{3}", d)
             return "%sh%sm%ss" % m.groups()
