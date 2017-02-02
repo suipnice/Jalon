c4350062756e9308caced088539bb62521729f6f
ppomedio <philippe.pomedio@unice.fr>
2017-02-02 16:48:42 +0100

    Création : contrôle de doublon éventuel

--- jalon.codekit/js/static/tag.js
+++ jalon.codekit/js/static/tag.js
@@ -118,23 +118,43 @@ function createTag( ) {
 
         event.preventDefault( );
 
-        var $form = $( this );
-        var $reveal = Foundation.utils.S( '#reveal-tags' );
-        var id = $form.find( 'input[name="tag_id"]' ).val( );
-        var title = $form.find( 'input[name="title"]' ).val( );
+        var $form = $( this ),
+            $reveal = Foundation.utils.S( '#reveal-tags' ),
+            $tagTitleContainer = $reveal.find( '#archetypes-fieldname-title' ),
+            $tagTitleErrorBox = $tagTitleContainer.find( '> .fieldErrorBox' ),
+            id = $form.find( 'input[name="tag_id"]' ).val( ),
+            title = $form.find( 'input[name="title"]' ).val( );
 
-        $.post( $form.attr( 'action' ), $form.serialize( ), null, 'html' ).done( function( data ) {
-            var html = $.parseHTML( data );
-            if ( $( html ).find( '.error' ).length ) {
-                $reveal.html( data );
-                revealInit( $reveal );
-            } else {
-                Foundation.utils.S( '#js-tag_filter' ).append( ' <li><a id="' + id + '" class="filter-button unselected"><i class="fa fa-circle no-pad"></i><i class="fa fa-circle-thin no-pad"></i> ' + title + '</a>' );
-                Foundation.utils.S( '#js-update_target' ).empty( ).html( data );
-                $reveal.foundation( 'reveal', 'close' );
-                setAlertBox( 'success', $form.data( 'success_msg_pre' ) + ' « ' + title + ' » ' + $form.data( 'success_msg_post' ) );
+        if ( ! _checkDuplicateTag( title ) ) {
+
+            if ( $tagTitleErrorBox.length ) {
+                $tagTitleErrorBox.remove( );
             }
-        } );
+
+            revealFormOnSubmitBehavior( $form );
+
+            $.post( $form.attr( 'action' ), $form.serialize( ), null, 'html' ).done( function( data ) {
+                var html = $.parseHTML( data );
+                if ( $( html ).find( '.error' ).length ) {
+                    $reveal.html( data );
+                    revealInit( $reveal );
+                } else {
+                    Foundation.utils.S( '#js-tag_filter' ).append( ' <li><a id="' + id + '" class="filter-button unselected"><i class="fa fa-circle no-pad"></i><i class="fa fa-circle-thin no-pad"></i> ' + title + '</a>' );
+                    Foundation.utils.S( '#js-update_target' ).empty( ).html( data );
+                    $reveal.foundation( 'reveal', 'close' );
+                    setAlertBox( 'success', $form.data( 'success_msg_pre' ) + ' « ' + title + ' » ' + $form.data( 'success_msg_post' ) );
+                }
+            } );
+
+        } else {
+
+            if ( $tagTitleErrorBox.length ) {
+                $tagTitleErrorBox.text( $form.data( 'error_msg_duplicate' ) );
+            } else {
+                $tagTitleContainer.prepend( '<div class="fieldErrorBox">'
+                    + $form.data( 'error_msg_duplicate' ) + '</div>' );
+            }
+        }
 
     } );
 }
